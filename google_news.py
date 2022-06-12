809175eec97badc11748dc1c7d45bd8ccc739069
la-strole <la-strole.sovaleto48@gmail.com>
2022-06-12 15:48:28 +0300

    Cahnge google_news parametrs

--- google_news.py
+++ google_news.py
@@ -6,7 +6,7 @@ from GoogleNews import GoogleNews
 
 class google_news:
     def __init__(self):
-        # set logging
+        # Set logging
         self.logger_gooogle_news = logging.getLogger('google_news')
         f_handler = logging.FileHandler('Telegram.log')
         f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -22,10 +22,10 @@ class google_news:
             return None
 
         try:
-            googlenews = GoogleNews(lang=f'{language}')
+            googlenews = GoogleNews(lang=f'{language}', period='5d', region=f'{language.upper()}')
             googlenews.search(f'{key_word}')
             first_link = googlenews.results()[0]
-            result = f'{first_link.get("title")}. \nДата: {first_link.get("date")}'
+            result = f'{first_link.get("title")}. \nДата: {first_link.get("date")}\nМедиа:{first_link.get("media")}'
         except Exception as e:
             self.logger_gooogle_news.exception(f"google_news: problem with google news. {e}")
             return None
@@ -35,5 +35,5 @@ class google_news:
 
 if __name__ == "__main__":
     instance = google_news()
-    print(instance.get_google_news('Новоград-Волинський'))
+    print(instance.get_google_news('Агутин', language='ru'))
 
